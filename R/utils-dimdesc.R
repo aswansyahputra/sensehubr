bc274bdf6bf5d9528588a92e6ea9da3e1938d024
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-03-20 23:22:38 +0700

    export dimdesc

---
+++ R/utils-dimdesc.R
@@ -0,0 +1,10 @@
+#' Description of dimension
+#'
+#' See \code{FactoMiner::\link[FactoMineR]{dimdesc}} for details.
+#'
+#' @name dimdesc
+#' @rdname dimdesc
+#' @keywords internal
+#' @export
+#' @importFrom FactoMineR dimdesc
+NULL
