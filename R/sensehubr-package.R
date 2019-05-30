7caeba36eb749942676476bf42ff641ca5e75eae
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-05-30 12:24:16 +0700

    add package-level manual and miscs

---
+++ R/sensehubr-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("attribute", "data", "model", "stats", "values", "statistic"))
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom utils data
+## usethis namespace: end
+NULL
