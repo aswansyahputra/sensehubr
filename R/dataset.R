e7baf025cce353f9689b9efd134e7877c69c2db0
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-06-01 13:50:57 +0700

    document dataset

---
+++ R/dataset.R
@@ -0,0 +1,14 @@
+#' Perfume QDA Consumers
+#'
+#' This dataset containing information about 12 perfumes that evaluated by 103 consumer panelists using predefined lexicons
+#'
+#' @format Dataframe dengan 142 baris dan 5 kolom
+#'
+#' \describe{
+#'   \item{consumer}{consumers' id}
+#'   \item{product}{perfumes}
+#'   \item{intensity-green}{lexicons regarding perfume}
+#' }
+#'
+#' @source Analyzing Sensory Data with R \url{www.sensorywithr.org}
+"perfume_qda_consumers"
