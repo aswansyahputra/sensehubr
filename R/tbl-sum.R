eb86dfada3e6c943ba3a90a46c33f4b29ac73f46
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-06-01 13:52:03 +0700

    improve printing method for global analysis

--- R/tbl-sum.R
+++ R/tbl-sum.R
@@ -12,7 +12,7 @@ tbl_sum.tbl_sensory_qda <- function(x){
 
 #' @export
 
-tbl_sum.tbl_sensory_qda_local <- function(x) {
+tbl_sum.tbl_sensory_local <- function(x) {
   c(
     "A sensory table" = meta_info(x, "method"),
     "Type" = "Local analysis",
@@ -20,22 +20,12 @@ tbl_sum.tbl_sensory_qda_local <- function(x) {
   )
 }
 
-#' @export
-
-print <- function(x) {
-  UseMethod("print")
-}
-
-#' @import tibble
-#' @export
 
 print.tbl_sensory_global <- function(x){
-  cat_subtle("# Sensory table\n")
+  print.tbl(x$eigenvalue)
   cat_subtle("#\n")
-  tibble:::print.tbl(x$eigenvalue)
+  print.tbl(x$product)
   cat_subtle("#\n")
-  tibble:::print.tbl(x$product)
-  cat_subtle("#\n")
-  tibble:::print.tbl(x$attribute)
+  print.tbl(x$attribute)
   invisible(x)
 }
