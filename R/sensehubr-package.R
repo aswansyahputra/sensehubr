ebd6705e08e081c3b2020c469e49aa1bb051590a
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-06-02 15:44:36 +0700

    clean up and rebuild pkgdown

--- R/sensehubr-package.R
+++ R/sensehubr-package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 "_PACKAGE"
 
-if (getRversion() >= "2.15.1")  utils::globalVariables(c("attribute", "data", "model", "stats", "values", "statistic", "cumulative.variance.percent", "parameter", "product", "variance.percent", "contribution", "dimension", "eigenvalue", "name"))
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("attribute", "data", "model", "stats", "values", "statistic", "cumulative.variance.percent", "parameter", "product", "variance.percent", "contribution", "dimension", "eigenvalue", "name", "p.value"))
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
