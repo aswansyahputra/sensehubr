f83c23626ec336e911428fe0e9ac2a573acb26b8
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-06-19 13:57:42 +0700

    ignore commited directory

--- .R/sensehubr-package.R
+++
@@ -1,7 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-utils::globalVariables(c(
-  ".", "Order", "Panelis", "Panelist", "Urutan", "attribute", "data", "dimension", "eigenvalue",
-  "means", "median", "model", "p.value", "percentage of variance", "probs", "product", "statistic", "stats", "value"
-))
